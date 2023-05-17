15c114f326123ea23e4e4569f16cde3b95153531
Gabriel Wachira <gabbyk.com@gmail.com>
2023-05-17 15:14:52 +0300

    Update builtins: pwd-getcwd

--- shell.h
+++ shell.h
@@ -1,14 +1,14 @@
 #ifndef SHELL_H
 #define SHELL_H
 
-#include<stdio.h>
+#include <stdio.h>
 #include <limits.h>
 #include <signal.h>
-#include<string.h>
-#include<stdlib.h>
-#include<unistd.h>
-#include<sys/types.h>
-#include<sys/wait.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include <stdarg.h>
 #include <errno.h>
 
